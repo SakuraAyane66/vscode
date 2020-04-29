450214a3b650e17494f8498939a419955efd1113
SakuraAyane66 <809653361@qq.com>
2020-04-30 01:26:01 +0800

    学习了vue中的is方法和ref，父子组件传值

--- index.js
+++
@@ -1,6 +0,0 @@
-first
-seconed
-third
-sakura
-ayane
-//第8天没学习日语了，git和chrome真香
---
+++ js/index.js
@@ -0,0 +1,8 @@
+first
+seconed
+third
+sakura
+ayane
+//第8天没学习日语了，git和chrome真香
+//今天肝了一波日语，过了一遍整个初级上，做完了那套课后习题。准确率还行。
+//组建的细节，用<tr is=""></tr> <li is=""></li> <option>等 is代替子组件替换某些bug
